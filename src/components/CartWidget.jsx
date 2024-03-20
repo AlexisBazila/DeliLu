5fafea54d2a91578e45b6ede34b775b79ee765de
AlexisBazila <alexisbazila@proton.me>
2024-03-20 13:08:31 -0300

    mejora CartWidget

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,9 +1,15 @@
 import '../../style/CartWidget.css';
 
-export const CartWidget = () =>{
-   return (
-    <>
-        <i class='bx bx-cart'>5</i>
-    </>
-    )
-}
+export const CartWidget = () => {
+	return (
+		<>
+			<button type="button" class="btn btn-primary position-relative">
+				<i class="bx bx-cart"></i>
+				<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+					5
+					<span class="visually-hidden">unread messages</span>
+				</span>
+			</button>
+		</>
+	);
+};
